6189463e55cdb622eaf35da3b4383cf9b480b458
Lukeslux <lucas.brnd@gmail.com>
2016-02-24 17:54:11 +0100

    Basic Letter Script

---
+++ ScrabbleBattle/Scripts/Letter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Letter : MonoBehaviour {
+	public GameObject charObject;
+	public char character;
+	private TextMesh textMesh;
+	private BoxCollider2D boxCollider;
+
+	// Use this for initialization
+	void Start () {
+		textMesh = charObject.GetComponent<TextMesh>();
+		boxCollider = this.GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		textMesh.text = ""+character;
+		
+		foreach (Touch touch in Input.touches) {
+			Vector3 wp = Camera.main.ScreenToWorldPoint(touch.position);
+			if (boxCollider.OverlapPoint(wp)) {
+				textMesh.text = "OUCH!";
+			}
+		}
+	}
+}
