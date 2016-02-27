e95861ddc76360b94b6bbedcf8e1b5eec3157bd5
Sietze Houwink <sietzehouwink@hotmail.com>
2016-02-27 14:06:09 +0100

    Project to demonstrate relevant networking.

---
+++ SharedObject/Assets/Move.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	// publicly accessible (zero by Unity default)
+	public Vector3 translation;
+	
+	// called once per frame
+	void Update () {
+		transform.Translate (translation);
+	}
+}
