423c377be48cfdb15fb4dbaa54105dbad00d5993
Lukeslux <lucas.brnd@gmail.com>
2016-02-29 00:00:53 +0100

    Final MVP

--- ScrabbleBattle/Assets/Scripts/Letter.cs
+++ ScrabbleBattle/Assets/Scripts/Letter.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿// Script for the clickable letter.
+using UnityEngine;
 using System.Collections;
 using UnityEngine.Networking;
 using System;
@@ -6,79 +7,22 @@ using System;
 public class Letter : NetworkBehaviour {
 	public GameObject charObject;
 	public GameObject background;
-	private BoxCollider2D boxCollider;
 	private SpriteRenderer sprite;
-	public char character;
 	[SyncVar]
-	public bool taken;
-    //eu
-    public WordManager sendDetailsForWord;
-    private int sent = 0;
-    //
+	public bool taken; // Whether or not this letter has been taken.
 
     // Initialisation.
     public void Start () {
-		boxCollider = this.GetComponent<BoxCollider2D>();
 		sprite = background.GetComponent<SpriteRenderer>();
-		setTaken(false);
+		this.taken = false;
 	}
 	
 	// Update is called once per frame.
 	public void Update () {
-		if (HasClicked()) {
-			OnClick();
-		}
 		if (taken) {
 			sprite.color = new Color(.4f, .4f, .4f);
-			Vector3 loc = transform.localPosition;
-			loc.x += 0.1f;
-			transform.localPosition = loc;
 		} else {
 			sprite.color = new Color(1f, 1f, 1f);
         }
 	}
-	
-	// Sets whether or not this Letter is taken.
-
-	public void setTaken(bool isTaken) {
-		this.taken = isTaken;
-    }
-	
-	// Called when this Letter has been clicked.
-
-	private void OnClick() {
-        //eu
-        if (taken == false && sent == 0)
-        {
-            sent = 1;
-            sendDetailsForWord.createWord(gameObject);
-        }
-        //
-        setTaken(!taken);
-    }
-	
-	// Checks for a mouseclick, use this on PC.
-	private bool HasClicked() {
-		if (Input.GetMouseButtonDown(0)) {
-			Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			if (boxCollider.OverlapPoint(wp)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-    // Checks for a touch.
-    private bool HasTouched()
-    {
-        foreach (Touch touch in Input.touches)
-        {
-            Vector3 wp = Camera.main.ScreenToWorldPoint(touch.position);
-            if (boxCollider.OverlapPoint(wp) && touch.phase == TouchPhase.Ended)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
 }
---
+++ ScrabbleBattle/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿// Server side script.
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		CheckClicked();
+	}
+	
+	// Called on the server by the client.
+	[Command]
+	void CmdSetTaken(string name) {
+		SetTaken(name);
+	}
+	
+	// Sets the letter with the given name to taken.
+	void SetTaken(string name) {
+		Letter l = GameObject.Find(name).GetComponent<Letter>();
+		l.taken = !l.taken;
+	}
+	
+	// Check if a letter has been clicked.
+	void CheckClicked() {
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+			RaycastHit hit;
+         
+			if( Physics.Raycast( ray, out hit, 100 ) ) {
+				string hitName = hit.transform.gameObject.name;
+				if (Network.isServer) {
+					SetTaken(name);
+				} else {
+					CmdSetTaken(hitName);
+				}
+			}
+		}
+	}
+	
+}
