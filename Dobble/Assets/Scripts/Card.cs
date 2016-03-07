c92a9a3fa5683f2682d9491b1a932659c35e53d1
Sietze Houwink <sietzehouwink@hotmail.com>
2016-03-07 17:45:48 +0100

    Initial project.

---
+++ Dobble/Assets/Scripts/Card.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour {
+
+	private int[] card;
+	
+	void Start () {
+		// hardcoded
+		card = new int[8];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
