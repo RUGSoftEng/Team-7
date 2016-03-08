1fba06bc4612be78d127fa34d30ec14d41b14b11
Sietze Houwink <sietzehouwink@hotmail.com>
2016-03-08 21:15:18 +0100

    Added parts of networking.

---
+++ Dobble/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Card cardPrefab;
+
+	Card card;
+
+	// Use this for initialization
+	void Start () {
+		this.card = (Card)Instantiate (cardPrefab);
+		this.card.transform.SetParent (this.transform);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 100)) {
+				int id = hit.transform.gameObject.GetInstanceID();
+
+				//this.card.SetCard (new int[] {0,3,2,4,6,7,8,2});
+			}
+		}
+	}
+}
