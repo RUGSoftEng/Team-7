9d67a78ce0fe8ef777f801366836d985177aa6de
Sietze Houwink <sietzehouwink@hotmail.com>
2016-03-09 12:36:02 +0100

    Improved structure.

--- Dobble/Assets/Scripts/Card.cs
+++ Dobble/Assets/Scripts/Card.cs
@@ -8,31 +8,39 @@ public class Card : MonoBehaviour {
 
 	public Symbol symbolPrefab;
 	public Sprite[] sprites;
-	
-	Symbol[] symbols;
+
+	private float radius;
+	private Symbol[] symbols; 
 
 	void Start () {
-		this.sprites = Resources.LoadAll<Sprite>("");
 
 		// hardcoded to 8
-		float radius = 0.302593388348611302909204224934f;
-		float factor = 1/(2*radius);
-		((Symbol)Instantiate (symbolPrefab, new Vector3 (-0.302593388348611302909204224933f*factor, -0.628341645367213738512227388956f*factor, 0), Quaternion.identity)).transform.SetParent(this.transform);
-		((Symbol)Instantiate (symbolPrefab, new Vector3 (0.302593388348611302909204224933f*factor, -0.628341645367213738512227388956f*factor, 0), Quaternion.identity)).transform.SetParent(this.transform);
-		((Symbol)Instantiate (symbolPrefab, new Vector3 (-0.679921171839088240043878874469f*factor, -0.155187570571975671990838057814f*factor, 0), Quaternion.identity)).transform.SetParent(this.transform);
-		((Symbol)Instantiate (symbolPrefab, new Vector3 (0.679921171839088240043878874469f*factor, -0.155187570571975671990838057814f*factor, 0), Quaternion.identity)).transform.SetParent(this.transform);
-		((Symbol)Instantiate (symbolPrefab, new Vector3 (0.000000000000000000000000000000f*factor, 0.000000000000000000000000000000f*factor, 0), Quaternion.identity)).transform.SetParent(this.transform);
-		((Symbol)Instantiate (symbolPrefab, new Vector3 (-0.545254445070410775447749861103f*factor, 0.434825910113495061957667559237f*factor, 0), Quaternion.identity)).transform.SetParent(this.transform);
-		((Symbol)Instantiate (symbolPrefab, new Vector3 (0.545254445070410775447749861103f*factor, 0.434825910113495061957667559237f*factor, 0), Quaternion.identity)).transform.SetParent(this.transform);
-		((Symbol)Instantiate (symbolPrefab, new Vector3 (0.000000000000000000000000000000f*factor,  0.697406611651388697090795775067f*factor, 0), Quaternion.identity)).transform.SetParent(this.transform);
+		this.radius = 0.302593388348611302909204224934f;
+		Vector2[] coordinates = new Vector2[8] {
+			new Vector2(-0.302593388348611302909204224933f, -0.628341645367213738512227388956f),
+			new Vector2(0.302593388348611302909204224933f, -0.628341645367213738512227388956f),
+			new Vector2(-0.679921171839088240043878874469f, -0.155187570571975671990838057814f),
+			new Vector2(0.679921171839088240043878874469f, -0.155187570571975671990838057814f),
+			new Vector2(0.000000000000000000000000000000f, 0.000000000000000000000000000000f),
+			new Vector2(-0.545254445070410775447749861103f, 0.434825910113495061957667559237f),
+			new Vector2(0.545254445070410775447749861103f, 0.434825910113495061957667559237f),
+			new Vector2(0.000000000000000000000000000000f, 0.697406611651388697090795775067f)
+		}; 
 
-		this.symbols = GetComponentsInChildren<Symbol> ();
+		this.sprites = Resources.LoadAll<Sprite>("");
+		this.symbols = new Symbol[8];
+		Symbol s;
+		for (int i = 0; i < 8; ++i) {
+			s = (Symbol) Instantiate (symbolPrefab, coordinates[i], Quaternion.identity);
+			s.transform.SetParent(this.transform);
+			s.radius = radius;
+			s.sprites = sprites;
+			this.symbols[i] = s;
+		}
 	}
 
 	public bool ContainsSymbol(int index) {
-		foreach (Symbol s in this.symbols) {
-			if (s.index == index) return true;
-		}
+		foreach (Symbol s in this.symbols) if (s.getIndex() == index) return true;
 		return false;
 	}
 
--- Dobble/Assets/Scripts/Symbol.cs
+++ Dobble/Assets/Scripts/Symbol.cs
@@ -4,21 +4,24 @@ using System;
 
 public class Symbol : MonoBehaviour {
 
-	public int index;
-
-	Sprite[] sprites;
-	SpriteRenderer spriteRenderer;
+	private int index;
+	public float radius;
+	public Sprite[] sprites;
+	private SpriteRenderer spriteRenderer;
 	
 	void Start () {
-		this.sprites = GetComponentInParent<Card> ().sprites;
 		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+
+	public int getIndex() {
+		return this.index;
 	}
 
 	public void SetSymbol(int index) {
 		this.index = index;
 		this.spriteRenderer.sprite = this.sprites[index];
 		Vector3 s = this.sprites[index].bounds.size;
-		float scale = 1.0f/Mathf.Max (s.x, s.y);
+		float scale = 1.0f/Mathf.Sqrt (s.x*s.x + s.y*s.y)*this.radius*2;
 		this.transform.localScale = new Vector3(scale, scale, scale);
 		this.transform.eulerAngles = new Vector3(0, 0, UnityEngine.Random.value*360);
 	}
