7921f3f64cba705debf2d38d8a74c5a43a672ace
Sietze Houwink <sietzehouwink@hotmail.com>
2016-03-09 15:32:16 +0100

    Updated constructor.

--- Dobble/Assets/Scripts/Player.cs
+++ Dobble/Assets/Scripts/Player.cs
@@ -9,9 +9,7 @@ public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		this.card = (Card)Instantiate (cardPrefab);
-		this.card.transform.SetParent (this.transform);
-
+		this.card = (Card)Instantiate (cardPrefab).Constructor(this.transform);
 	}
 	
 	// Update is called once per frame
