db5b95a380aeff13f94c6e80b1667774cdf45557
Luc van den Brand <Lukeslux@users.noreply.github.com>
2016-04-15 15:21:24 +0200

    Made Lobby skeleton

--- Matchimals/Assets/Scripts/Networking/Lobby.cs
+++ Matchimals/Assets/Scripts/Networking/Lobby.cs
@@ -4,8 +4,40 @@ using UnityEngine;
 using System.Collections;
 
 public class Lobby : MonoBehaviour {
+	private GUISkin menuSkin;
 	
-	public void OnGUI () {
-		// Draw the lobby.
+	public void Start() {
+		this.menuSkin = Resources.Load<GUISkin>("Menu/MainMenu");
 	}
+	
+	// Draws the GUI.
+	public void OnGUI () {
+		GUI.skin = menuSkin;
+		
+		Player[] players = getPlayers();
+		foreach (Player player in players) {
+			//Draw the waiting players.
+		}
+		//Draw the ready button.
+	}
+	
+	private Player[] getPlayers() {
+		return GameObject.FindObjectsOfType(typeof(Player)) as Player[];
+	}
+	
+	// Checks if all players are ready.
+	private bool allReady(Player[] players) {
+		bool ready = true;
+		foreach (Player player in players) {
+			// TODO: create ready function in Player. 
+			//ready = ready && player.isReady();
+		}
+		return ready;
+	}
+	
+	// Start the game with all the players in it.
+	private void startGame() {
+		
+	}
+	
 }
