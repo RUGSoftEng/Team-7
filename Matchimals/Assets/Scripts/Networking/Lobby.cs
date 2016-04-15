85209606bf54e43559989e33c10e912ed995e0db
Luc van den Brand <Lukeslux@users.noreply.github.com>
2016-04-15 15:00:19 +0200

    Made Networking folder to keep things organised

---
+++ Matchimals/Assets/Scripts/Networking/Lobby.cs
@@ -0,0 +1,11 @@
+﻿// The Lobby script is created when a player is hosting or joined a game, 
+// it shows the waiting players.
+using UnityEngine;
+using System.Collections;
+
+public class Lobby : MonoBehaviour {
+	
+	public void OnGUI () {
+		// Draw the lobby.
+	}
+}
