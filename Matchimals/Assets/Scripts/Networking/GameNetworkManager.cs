5f197c96d94afe41dcdef01b904b9c32b5f46be2
Luc van den Brand <lucas.brnd@gmail.com>
2016-04-22 11:44:20 +0200

    Fix

--- Matchimals/Assets/Scripts/Networking/GameNetworkManager.cs
+++ Matchimals/Assets/Scripts/Networking/GameNetworkManager.cs
@@ -5,9 +5,12 @@ using System.Collections;
 public class GameNetworkManager : NetworkManager {
     
     public override void OnClientDisconnect(NetworkConnection conn) {
-        Lobby lobby = Lobby.FindObjectOfType<Lobby>() as Lobby;
+        Lobby lobby = FindObjectOfType<Lobby>() as Lobby;
         if (lobby != null) {
             lobby.GoBack();
+            MainMenu menu = FindObjectOfType<MainMenu>() as MainMenu;
+            Debug.Assert(menu != null);
+            menu.hostIP = null;
         }
     }
 }
