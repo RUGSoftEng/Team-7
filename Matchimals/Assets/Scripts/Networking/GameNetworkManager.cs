15e3f9d5f13bd25030b6d69121e63259569c8c91
Luc van den Brand <lucas.brnd@gmail.com>
2016-04-22 11:09:49 +0200

    Client connect

--- Matchimals/Assets/Scripts/Networking/GameNetworkManager.cs
+++ Matchimals/Assets/Scripts/Networking/GameNetworkManager.cs
@@ -7,4 +7,9 @@ public class GameNetworkManager : NetworkManager {
     public new void OnClientDisconnect(NetworkConnection conn) {
         Debug.Log("Doe");
     }
+
+    public new void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log("Moi");
+    }
 }
