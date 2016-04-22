eba090e4a7643a009bae166a1acffedefd7c99e6
Luc van den Brand <lucas.brnd@gmail.com>
2016-04-22 10:58:31 +0200

    Updated networkmanager

---
+++ Matchimals/Assets/Scripts/Networking/GameNetworkManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class GameNetworkManager : NetworkManager {
+    
+    public new void OnClientDisconnect(NetworkConnection conn) {
+        Debug.Log("Doe");
+    }
+}
