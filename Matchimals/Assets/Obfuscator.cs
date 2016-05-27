18c154401ca74c50c3ead83dd6c4d563a6050e26
Luc van den Brand <lucas.brnd@gmail.com>
2016-05-27 11:53:33 +0200

    Added gameobject that can make the screen darker, for gameover

---
+++ Matchimals/Assets/Obfuscator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Renderer))]
+public class Obfuscator : MonoBehaviour {
+    public float speed = 0.01f;
+    private float opacity;
+    private Material material;
+
+	// Get the color of the material.
+	public void OnEnable () {
+        this.material = gameObject.GetComponent<Renderer>().material;
+        this.opacity = material.color.a;
+        Debug.Assert(material != null);
+	}
+	
+	// Change the opacity based on obfuscation goal.
+	public void Update () {
+        float timedSpeed = speed * Time.deltaTime;
+        Color c = material.color;
+        if (c.a != opacity) {
+            float diff = c.a - opacity;
+            if (Mathf.Abs(diff) > speed) {
+                if (diff < 0)
+                    c.a += speed;
+                else
+                    c.a -= speed;
+            } else {
+                c.a = this.opacity;
+            }
+            material.color = c;
+        }
+    }
+
+    public void Obfuscate(float opacity){
+        this.opacity = opacity;
+    }
+}
